b9db258a87e5c560a442efce7b13270cd580449f
fanggodzzz <phan7085@mylaurier.ca>
2026-03-21 22:17:01 -0400

    Q-learning backbone

--- q_learning_parking.py
+++ q_learning_parking.py
@@ -9,7 +9,10 @@ import random
 # ---------------------------------------------------------
 # Sample test input
 # ---------------------------------------------------------
-LOTS = ["P1", "P2", "P3"]
+
+
+LOTS = {"1": 21, "2": 22, "3": 23, "4": 24, "5": 25} # Dictionary mapping parking lots to nodes
+p_avail = [1, 1, 1, 1, 1]  # parking availability for each parking lot
 
 # Drive time to each parking lot at each time step t
 drive_time = {
@@ -42,7 +45,7 @@ avail = {
 # ---------------------------------------------------------
 # Constraints and weights
 # ---------------------------------------------------------
-W_MAX = 0.5
+W_MAX = 10
 LAMBDA_WALK = 10.0
 LAMBDA_PRICE = 1.0
 PENALTY = 10**6
@@ -95,3 +98,68 @@ def is_terminal_state(t):
 
 
 
+
+
+def create_environment():
+    """
+    Create dynamic environment for Q-learning simulation
+
+    * Network: 
+        - Graph with 21 nodes + 5 parking nodes
+            + Node 0:         starting node
+            + Node 1 - 20:    intersections
+            + Node 21 - 25:   parking lots
+    * Parking lots:
+        - Parking lot 1 - 3 (Node 21 - 23): Within W_distance
+        - Parking lot 4 - 5 (Node 24 - 25): Out of W_MAX distance
+        - Price for each lots - randomize from 10 - 15 (dynamically change)
+        - Availability: 0 or 1 (dynamically change)   
+    * Traffic (Undirected Edges):
+        - Fixed weight edges (randomly choose from 5 - 10)
+        - During simulation, traffic dynamicity: 
+            + Low:     weight * 1
+            + Medium:  weight * 2
+            + High:    weight * 5
+    """
+    pass
+
+def change_environment():
+    """
+    Create dynamic environment for Q-learning simulation
+
+    * Network: 
+        - Graph with 21 nodes + 5 parking nodes
+            + Node 0: starting node
+            + Node 1 - 20: intersections
+            + Node 21 - 25: parking lots
+    * Parking lots:
+        - Parking lot 1 - 3 (Node 21 - 23): Within W_distance
+        - Parking lot 4 - 5 (Node 24 - 25): Out of W_MAX distance
+        - Price for each lots - randomize from 10 - 20 (dynamically change)
+        - Availability: 0 or 1 (dynamically change)   
+    * Traffic (Undirected Edges):
+        - Fixed weight edges (randomly choose from 5 - 10)
+        - During simulation, traffic dynamicity: 
+            + Low   (0): weight * 1
+            + Medium(1): weight * 2
+            + High  (2): weight * 5
+    """
+    pass
+
+def q_learning_simulate():
+    """
+
+    """
+    pass
+
+def print_result():
+    pass
+
+def main():
+    create_environment()
+    q_learning_simulate()
+    print_result()
+    pass
+
+if __name__ == "__main__":
+    main()
